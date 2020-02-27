2b8e1120ce00ce434b277eda32cf5ecaf4ce63c0
G Mason <gmason0702@gmail.com>
2020-02-27 10:49:53 -0500

    secondCommit

---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ElevenNote.Models
+{
+    public class NoteListItem
+    {
+        [Key]
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
